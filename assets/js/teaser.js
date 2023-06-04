7185bea8657229a35c7c76a2704fa551dc3f3b14
LovaHenin <ratovolova@yahoo.com>
2023-06-04 22:47:16 +0200

    Ajout logo et text

--- assets/js/teaser.js
+++ assets/js/teaser.js
@@ -1,17 +1,27 @@
-var Affiche=document.getElementById("timer");
- function Rebour() {
- var date1 = new Date();
- var date2 = new Date ("Jun 31, 2023 00:00:00");
- var sec = (date2 - date1) / 1000;
- var n = 24 * 3600;
- if (sec > 0) {
- j = Math.floor (sec / n);
- h = Math.floor ((sec - (j * n)) / 3600);
- mn = Math.floor ((sec - ((j * n + h * 3600))) / 60);
- sec = Math.floor (sec - ((j * n + h * 3600 + mn * 60)));
- Affiche.innerHTML = "" + j +" j "+ h +" h "+ mn +" min "+ sec + " s";
- window.status = "Temps restant : " + j +" j "+ h +" h "+ mn +" min "+ sec + " s ";
- }
- tRebour=setTimeout ("Rebour();", 1000);
- }
- Rebour();
+//var Affiche = document.getElementById("compte");
+var jour = document.getElementById("jour");
+var heure = document.getElementById("heure");
+var min = document.getElementById("min");
+var seconde = document.getElementById("sec");
+function Rebour() {
+    var date1 = new Date();
+    var date2 = new Date("Jun 31, 2023 00:00:00");
+    var sec = (date2 - date1) / 1000;
+    var n = 24 * 3600;
+    if (sec > 0) {
+        j = Math.floor(sec / n);
+        h = Math.floor((sec - (j * n)) / 3600);
+        mn = Math.floor((sec - ((j * n + h * 3600))) / 60);
+
+        sec = Math.floor(sec - ((j * n + h * 3600 + mn * 60)));
+
+        jour.innerHTML = "" + j + " j " ;
+        heure.innerHTML="" + h + " h ";
+        min.innerHTML="" + mn + " min ";
+        seconde.innerHTML="" + sec + " s ";
+
+        window.status = "Temps restant : " + j + " j " + h + " h " + mn + " min " + sec + " s ";
+    }
+    tRebour = setTimeout("Rebour();", 1000);
+}
+Rebour();
--- teaser.php
+++ teaser.php
@@ -8,25 +8,51 @@
     <title>Teaser</title>
     <link rel="stylesheet" href="assets/bootstrap/scss/bootstrap.css">
     <script src="assets/bootstrap/js/bootstrap.bundle.min.js"></script>
+
     <link rel="stylesheet" href="assets/css/teaser.css">
 </head>
 
 <body>
-    <main class="d-flex flex-column justify-content-center align-items-center">
-        <label class="text-white" id="Compte"></label>
+    <main>
+        <!-- <label class="text-white" id="compte"></label> -->
+        <div class="container">
+
         
+                <img src="assets/img/starisland.png" alt="logo">
+           
+            <div class="titre">
+                <p>Rejoignez-nous sur Star Island et préparez-vous à devenir une légende. <br/>Le compte à rebours commence maintenant...</p>
+            </div>
+        </div>
+        <div class="affiche">
+
+            <label class="text-white" id="jour"></label>
+            <label class="text-white" id="heure"></label>
+            <label class="text-white" id="min"></label>
+            <label class="text-white" id="sec"></label>
+
+        </div>
+
+
+        <div class="gradient"></div>
         <div class="reseaux-sociaux">
             <a id="facebook" class="reseaux facebook" href="">
                 <img src="assets/img/icons8-facebook.png" alt="facebook">
             </a>
             <a id="tiktok" class="reseaux" href="">
-                <img src="assets/img/icons8-tiktok.png" alt="tiktok">
+                <img src="assets/img/Logo_tiktok.png" alt="tiktok">
             </a>
             <a id="twitter" class="reseaux" href="">
                 <img src="assets/img/icons8-twitter.png" alt="twitter">
             </a>
             <a id="youtube" class="reseaux" href="">
                 <img src="assets/img/icons8-youtube.png" alt="youtube">
+            </a>
+            <a id="twitch" class="reseaux" href="">
+                <img src="assets/img/logo_twitch.png" alt="twitch">
+            </a>
+            <a id="instagramme" class="reseaux" href="">
+                <img src="assets/img/logo_Instagram.png" alt="instagramme">
             </a>
             <a id="discorde" class="reseaux" href="">
                 <img src="assets/img/icons8-discorde.png" alt="discorde">
@@ -35,26 +61,7 @@
     </main>
 
 
-    <script type="text/javascript">
-        var Affiche = document.getElementById("Compte");
-
-        function Rebour() {
-            var date1 = new Date();
-            var date2 = new Date("jun 30, 2023 00:00:00");
-            var sec = (date2 - date1) / 1000;
-            var n = 24 * 3600;
-            if (sec > 0) {
-                j = Math.floor(sec / n);
-                h = Math.floor((sec - (j * n)) / 3600);
-                mn = Math.floor((sec - ((j * n + h * 3600))) / 60);
-                sec = Math.floor(sec - ((j * n + h * 3600 + mn * 60)));
-                Affiche.innerHTML = j + " jours " + h + " h " + mn + " m " + sec + " s";
-                window.status = "Temps restant : " + j + " j " + h + " h " + mn + " min " + sec + " s ";
-            }
-            tRebour = setTimeout("Rebour();", 1000);
-        }
-        Rebour();
-    </script>
+    <script src="assets/js/teaser.js"> </script>
 </body>
 
 </html>
